98ce6b40bb529ef539db2ff48cb9461859df04d7
MAC C2 <imac5@LeaJ.local>
2018-03-14 17:00:26 +0100

    formulaire insctiption + branchement clic

--- app/src/main/java/com/example/imac5/liste/MainActivity.java
+++ app/src/main/java/com/example/imac5/liste/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.imac5.liste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button buttonConnect;
+    public Button buttonSubscribe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        buttonConnect = (Button) findViewById(R.id.connect);
+//        buttonConnect.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this,StandingsActivity.class));
+//            }
+//        });
+
+        buttonSubscribe = (Button) findViewById(R.id.inscription);
+        buttonSubscribe.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,InscriptionActivity.class));
+            }
+        });
+
     }
 }
