0263fa524acb113ca89a6153f5d5930a2605e077
MAC C2 <imac5@LeaJ.local>
2018-03-26 17:04:13 +0200

    mise à jour inscription activity

--- app/src/main/java/com/example/imac5/liste/InscriptionActivity.java
+++ app/src/main/java/com/example/imac5/liste/InscriptionActivity.java
@@ -12,6 +12,10 @@ public class InscriptionActivity extends AppCompatActivity {
     public Button buttonConnect;
     public Button buttonSubscribe;
     public EditText txtName;
+    public EditText txtEmail;
+    public EditText txtAge;
+    public EditText txtTel;
+
     public Button buttonValid;
 
     @Override
@@ -20,16 +24,16 @@ public class InscriptionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_inscription);
 
         txtName = (EditText) findViewById(R.id.name);
-        txtName = (EditText) findViewById(R.id.email_1);
-        txtName = (EditText) findViewById(R.id.age);
-        txtName = (EditText) findViewById(R.id.tel);
+        txtEmail = (EditText) findViewById(R.id.email_1);
+        txtAge = (EditText) findViewById(R.id.age);
+        txtTel = (EditText) findViewById(R.id.tel);
 
         buttonValid = (Button) findViewById(R.id.valid) ;
         buttonValid.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if (validateForm()==1) {
                     // ici shareobject
-                    startActivity(new Intent(InscriptionActivity.this, InscriptionActivity.class));
+                    startActivity(new Intent(InscriptionActivity.this, WelcomeActivity.class));
                 }else{
                     startActivity(new Intent(InscriptionActivity.this, ErrorActivity.class));
                 }
@@ -44,37 +48,16 @@ public class InscriptionActivity extends AppCompatActivity {
         if (txtName.getText().length()==0) {
             retour = 0;
         }
-
-        return retour;
-    }
-
-    public int validateForm() {
-        int retour = 1;
-
-        if (txtName.getText().length()==0) {
+        if (txtEmail.getText().length()==0) {
             retour = 0;
         }
-
-        return retour;
-    }
-
-    public int validateForm() {
-        int retour = 1;
-
-        if (txtName.getText().length()==0) {
+        if (txtAge.getText().length()==0) {
             retour = 0;
         }
-
-        return retour;
-    }
-
-    public int validateForm() {
-        int retour = 1;
-
-        if (txtName.getText().length()==0) {
+        if (txtTel.getText().length()==0) {
             retour = 0;
         }
-
         return retour;
     }
+
 }
