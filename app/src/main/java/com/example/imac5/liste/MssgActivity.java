1093c66c064a5b683a0920416099d355e1f1d597
MAC C2 <gigotiris@gmail.com>
2018-05-17 18:47:09 +0200

    livraison

--- app/src/main/java/com/example/imac5/liste/MssgActivity.java
+++ app/src/main/java/com/example/imac5/liste/MssgActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,15 +13,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOError;
 import java.io.IOException;
-import java.net.Socket;
 import java.net.URISyntaxException;
-import java.util.function.IntUnaryOperator;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -31,18 +33,31 @@ public class MssgActivity extends AppCompatActivity {
     public Socket mSocket;
     public Activity me;
     public OkHttpClient client;
+    public Button buttonEnvoyer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mssg);
-        me = this;
+
+        buttonEnvoyer = findViewById(R.id.envoyer);
+        buttonEnvoyer.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sendMessage();
+                    }
+                }
+        );
+
 
         buttonDeconnect = (Button) findViewById(R.id.deconnect);
         buttonDeconnect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {startActivity(new Intent(MssgActivity.this, WelcomeActivity.class));
+            public void onClick(View view) {startActivity(new Intent(MssgActivity.this, ConnexionActivity.class));
             }
-                                           });
+        });
+
+        me = this;
 
         try {
             mSocket = IO.socket("https://esd-b1-messenger-project.glitch.me/");
@@ -72,23 +87,23 @@ public class MssgActivity extends AppCompatActivity {
                 }
             });
 
-            mSocket.on("liste", new Emitter.Listener() {
-                @Override
-                public void call(final Object... args) {
-                    me.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            JSONObject data = (JSONObject) args[0];
-
-                            Toast.makeText(getApplicationContext(),">>" + data.toString(), Toast.LENGTH_LONG).show();
-                        }
-                    });
-                }
-            });
-
         } catch (URISyntaxException e) {
             Log.wtf("debug",e.toString());
         }
+
+        mSocket.on("liste", new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                me.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject data = (JSONObject) args[0];
+
+                        Toast.makeText(getApplicationContext(),">>" + data.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
 
         client = new OkHttpClient();
 
@@ -97,8 +112,8 @@ public class MssgActivity extends AppCompatActivity {
 
             String strMessage = "";
             for (int i=0; i<obj.length(); i++) {
-                JSONObject message = obj.getJSONObject(i);
-                strMessage += "-" + message.getString("email") + "/" + message.getString("message") + "\n";
+                JSONObject jsmessage = obj.getJSONObject(i);
+                strMessage += "-" + jsmessage.getString("email") + "/" + jsmessage.getString("message") + "\n";
             }
 
             ((TextView) findViewById(R.id.intro)).setText(strMessage);
@@ -110,8 +125,25 @@ public class MssgActivity extends AppCompatActivity {
 
     }
 
-}
+    public void sendMessage() {
+        EditText txtMessage = (EditText) findViewById(R.id.message);
+        String message = txtMessage.getText().toString().trim();
 
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("message", message);
+            jsonObject.put("email", "test appli");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (!TextUtils.isEmpty(message)) {
+            txtMessage.setText("");
+            mSocket.emit("new message", jsonObject);
+
+        }
+        ;
+    }
 
     public String getMessage()
     {
@@ -129,3 +161,6 @@ public class MssgActivity extends AppCompatActivity {
         }
         return "";
     }
+
+}
+
